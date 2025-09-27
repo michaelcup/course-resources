9ca021691d470274981d22da8b131ee8bd5dae1f
michaelcup <120781270+michaelcup@users.noreply.github.com>
2025-09-27 00:02:46 -0400

    Update progress-bar.js

--- progress-bar.js
+++ progress-bar.js
@@ -67,13 +67,6 @@ class ProgressBar {
             <div class="theme-toggle">
                 <input class="toggle-checkbox" type="checkbox" id="themeToggleCheckbox">
                 <div class="toggle-slot">
-                    <div class="sun-icon-wrapper">
-                        <div class="iconify sun-icon" data-icon="feather-sun" data-inline="false"></div>
-                    </div>
-                    <div class="toggle-button"></div>
-                    <div class="moon-icon-wrapper">
-                        <div class="iconify moon-icon" data-icon="feather-moon" data-inline="false"></div>
-                    </div>
                 </div>
             </div>
         `;
